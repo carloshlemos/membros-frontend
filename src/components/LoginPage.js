5a8169dba41c53212d439f51e62e6bb4fd698c75
Carlos Henrique Lemos <chenriquelemos@gmail.com>
2026-01-17 21:33:22 -0300

    Adição da tela de login.

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,13 +1,19 @@
 import { useAuthContext } from "@asgardeo/auth-react";
 import React from "react";
+import Header from "./Header";
+import './LoginPage.css';
 
 const LoginPage = () => {
     const { signIn } = useAuthContext();
 
     return (
-        <div className="container">
-            <h1>Login</h1>
-            <button className="btn btn-primary" onClick={ () => signIn() }>Login</button>
+        <div className="login-page">
+            <Header />
+            <div className="login-container">
+                <h2>Acesso ao Sistema</h2>
+                <p>Para continuar, por favor, autentique-se usando sua conta.</p>
+                <button className="btn btn-primary" onClick={ () => signIn() }>Entrar</button>
+            </div>
         </div>
     );
 };
